fdea675b702010d0c6c7231db586c3bbcff2f757
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2025-10-28 21:30:53 +0530

    cors added

--- api/index.js
+++ api/index.js
@@ -5,17 +5,23 @@ const authRoutes = require("../routes/auth");
 const todoRoutes = require("../routes/todo");
 require("dotenv").config();
 
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
+
+
 
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log("MongoDB connected successfully"))
-.catch(err => console.error("MongoDB connection error:", err));
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 // Sample GET endpoint
 app.get("/api/hello", (req, res) => {
