8a3ab70570782c36d2626fddbbbb3095b2995d7d
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2025-10-28 14:47:30 +0530

    db connection

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 
 app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected successfully"))
+.catch(err => console.error("MongoDB connection error:", err));
 
 // Sample GET endpoint
 app.get("/api/hello", (req, res) => {
