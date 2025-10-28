ba4a6d5cadbe153596358ee6ac1e8292a54de0bd
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2025-10-28 14:12:30 +0530

    index changed

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const { Server } = require('socket.io');
+const { index } = require('socket.io');
 const path = require('path');
 const authRoutes = require('../routes/auth');
 const userRoutes = require('../routes/user');
@@ -14,8 +14,8 @@ const User = require('../models/User');
 const Message = require('../models/Message');
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: '*' } });
+const index = http.createindex(app);
+const io = new index(index, { cors: { origin: '*' } });
 
 app.use(cors());
 app.use(express.json({ limit: '20mb' }));
@@ -33,7 +33,7 @@ io.on('connection', (socket) => {
 
   socket.on('user_online', async ({ userId }) => {
     online.set(userId, socket.id);
-    try { await User.findByIdAndUpdate(userId, { online: true }); } catch(e){}
+    try { await User.findByIdAndUpdate(userId, { online: true }); } catch (e) { }
     io.emit('online_users', Array.from(online.keys()));
   });
 
@@ -56,14 +56,14 @@ io.on('connection', (socket) => {
       await Message.findByIdAndUpdate(messageId, { seen: true });
       const msg = await Message.findById(messageId);
       io.to(msg.room).emit('message_seen', { messageId, by: userId });
-    } catch(e){}
+    } catch (e) { }
   });
 
   socket.on('disconnect', async () => {
     for (const [uid, sid] of online.entries()) {
       if (sid === socket.id) {
         online.delete(uid);
-        await User.findByIdAndUpdate(uid, { online: false }).catch(()=>{});
+        await User.findByIdAndUpdate(uid, { online: false }).catch(() => { });
         io.emit('online_users', Array.from(online.keys()));
         break;
       }
@@ -72,8 +72,8 @@ io.on('connection', (socket) => {
   });
 });
 
-mongoose.connect(process.env.MONGO_URI).then(()=>{
+mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log('MongoDB connected');
   const port = process.env.PORT || 5000;
-  server.listen(port, ()=>console.log('Server listening on', port));
-}).catch(err=>console.error(err));
+  index.listen(port, () => console.log('index listening on', port));
+}).catch(err => console.error(err));
