d6a5b6ba46a2bbf923e1decf52fb7cc2fab90dcc
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2025-10-30 15:00:45 +0530

    env changed

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,11 @@
 const admin = require("firebase-admin");
+const path = require("path");
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    }),
-  });
-}
+// Get absolute path to the service account file
+const serviceAccount = require(path.join(__dirname, "serviceAccountKey.json"));
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
 
 module.exports = admin;
